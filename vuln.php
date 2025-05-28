7948552103027d2bb7ff06aecb5822558a610dfa
pranav10780 <pranavsuresh107@gmail.com>
2025-05-28 21:43:06 +0530

    Revert "added two parameters"
    This reverts commit 97b12d8980a50e0355a2ac80f2455959290647c9.
    
    reveting old mistakes

--- vuln.php
+++ vuln.php
@@ -1,21 +1,14 @@
 <?php
-if (isset($_GET['ip']) && isset($_GET['name'])) {
-    $name = $_GET['name'];
+if (isset($_GET['ip'])) {
     $ip = $_GET['ip'];
-    
-    echo "<pre>";
-    echo "Name: $name\n\n";
-    
-    $cmd = "ping -c 2 $ip";
-    echo "Command: $cmd\n\n";
+    $cmd = "ping -c 2 " . $ip;
+    echo "<pre>Command: $cmd\n\n";
     system($cmd);
     echo "</pre>";
 } else {
     echo '<form method="GET">
-            Name: <input type="text" name="name"><br>
-            Enter IP to ping: <input type="text" name="ip"><br>
+            Enter IP to ping: <input type="text" name="ip">
             <input type="submit" value="Ping">
           </form>';
 }
 ?>
-
